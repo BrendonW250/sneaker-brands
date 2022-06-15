05ca9404f6f41e6f7c2072dd70025b4a283a5494
BrendonW250 <brendonwright16@gmail.com>
2022-06-15 10:58:40 -0400

    sneaker try

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// client side js
+
+alert('its working')
+
+
+// console.log('hi')
+
+
+
+// function getFetch(){
+//     const url = 'https://thecocktaildb.com/api/json/v1/1/random.php'
+
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => {
+//             console.log(data)
+//         })
+
+//         .catch(err => {
+//             console.log(`error ${err}`)
+//         })
+// }
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+// creation of an object to be used to respond
+const sneakers = {
+    'Jordan': {
+        'brand': 'Jordan',
+        'nameOfShoe': 'Bred 11',
+        'price': '$350'
+    },
+
+    'Nike': {
+        'brand': 'Nike',
+        'nameOfShoe': 'Kyrie 2',
+        'price': '$220'
+    },
+
+    'Adidas': {
+        'brand': 'Adidas',
+        'nameOfShoe': 'T-Mac',
+        'price': '$200'
+    },
+
+    'Unknown': {
+        'brand': 'unknown',
+        'nameOfShoe': 'unknown',
+        'price': 'unknown'
+    }
+}
+
+
+// set up server to hear request w/ route and response
+app.get('/', (request,response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+// creating a different path
+// route is tailored to the user typing in a sneaker brand name
+app.get('/api/:sneakerBrand', (request,response) => {
+    // console.log(request.params.sneakerBrand)
+
+    const sneakerBrandName = request.params.sneakerBrand
+
+    // conditional on whether the brand is within the object (sneakers)
+    // if so give the info of that sneakerbrand
+    // using square bracket in the event of a space for property name
+    // if entered sneaker brand exists in query parameter respond with that brands info
+    if (sneakers[sneakerBrandName]){
+        response.json(sneakers[sneakerBrandName])
+    }else {
+        response.json(sneakers['Unknown'])
+    }
+
+
+    // response.json(sneakers)
+})
+
+// telling the app to listen
+app.listen(PORT, () => {
+    console.log(`The server is running on ${PORT}!`)
+})
+
